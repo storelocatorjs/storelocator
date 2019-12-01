70402a1b41edd00da2588c18303039bbba711549
Yoriiis <joris.daniel@gmail.com>
2019-12-01 12:09:55 +0100

    Update indent and rebuild

--- src/storelocator/js/templates/info-window.js
+++ src/storelocator/js/templates/info-window.js
@@ -6,35 +6,35 @@ import svgRoute from '../../svg/route.svg'
  */
 export default function ({store, origin}) {
 	return `
-        <div class="storelocator-infoWindow">
-            ${store.picture
-                ? `<span class="storelocator-pictureStore">
-                    <img src="${store.picture}" alt="${store.title}" />
-                </span>`
-            : ``}
-            <div class="storelocator-detailStore">
-                ${store.title
-                    ? `<span class="storelocator-detailStoreTitle">${store.index + 1}. ${store.title}</span>`
-                : ``}
-                <a href="http://www.google.fr/maps/dir/${origin}/${store.lat},${store.lng}" title="See the itinerary on Google Maps" target="_blank" class="storelocator-detailStoreDistance">
-                    <span>${store.distance.toFixed(2)}km</span>
-                    ${svgRoute}
-                    </a>
-                ${store.address
-                    ? `<span class="storelocator-detailStoreAddress">${store.address}</span>`
-                : ``}
-                ${store.zipcode
-                    ? `<span class="storelocator-detailStoreZipcode">${store.zipcode}</span>`
-                : ``}
-                ${store.city
-                    ? `<span class="storelocator-detailStoreCity">${store.city}</span>`
-                : ``}
-                ${store.phone
-                    ? `<span class="storelocator-detailStorePhone"><a href="tel:${store.phone}" title="Call">${store.phone}</a></span>`
-                : ``}
-                ${typeof store.link !== 'undefined'
-                ? `<a href="${store.link}" title="Visit website" target="_blank" class="storelocator-detailStoreUrl">${store.link}</a>`
-                : ``}
-            </div>
-        </div>`
+		<div class="storelocator-infoWindow">
+			${store.picture
+				? `<span class="storelocator-pictureStore">
+					<img src="${store.picture}" alt="${store.title}" />
+				</span>`
+			: ``}
+			<div class="storelocator-detailStore">
+				${store.title
+					? `<span class="storelocator-detailStoreTitle">${store.index + 1}. ${store.title}</span>`
+				: ``}
+				<a href="http://www.google.fr/maps/dir/${origin}/${store.lat},${store.lng}" title="See the itinerary on Google Maps" target="_blank" class="storelocator-detailStoreDistance">
+					<span>${store.distance.toFixed(2)}km</span>
+					${svgRoute}
+					</a>
+				${store.address
+					? `<span class="storelocator-detailStoreAddress">${store.address}</span>`
+				: ``}
+				${store.zipcode
+					? `<span class="storelocator-detailStoreZipcode">${store.zipcode}</span>`
+				: ``}
+				${store.city
+					? `<span class="storelocator-detailStoreCity">${store.city}</span>`
+				: ``}
+				${store.phone
+					? `<span class="storelocator-detailStorePhone"><a href="tel:${store.phone}" title="Call">${store.phone}</a></span>`
+				: ``}
+				${typeof store.link !== 'undefined'
+				? `<a href="${store.link}" title="Visit website" target="_blank" class="storelocator-detailStoreUrl">${store.link}</a>`
+				: ``}
+			</div>
+		</div>`
 }
--- src/storelocator/js/templates/sidebar-item-result.js
+++ src/storelocator/js/templates/sidebar-item-result.js
@@ -6,27 +6,27 @@ import svgRoute from '../../svg/route.svg'
  */
 export default function ({store, origin}) {
 	return `
-        <li class="storelocator-sidebarResultsListItem" data-category="${store.category}">
-            <div class="storelocator-detailStore">
-                ${store.title
-                    ? `<span class="storelocator-detailStoreTitle"><a href="" title="See on the map" class="store-center-marker-js" data-marker-index="${store.index}">${store.index + 1}. <span>${store.title}</span></a></span>`
-                : ``}
-                <a href="http://www.google.fr/maps/dir/${origin}/${store.lat},${store.lng}" title="See the itinerary on Google Maps" target="_blank" class="storelocator-detailStoreDistance">
-                    <span>${store.distance.toFixed(2)}km</span>
-                    ${svgRoute}
-                </a>
-                ${store.address
-                    ? `<span class="storelocator-detailStoreAddress">${store.address}</span>`
-                : ``}
-                ${store.zipcode
-                    ? `<span class="storelocator-detailStoreZipcode">${store.zipcode}</span>`
-                : ``}
-                ${store.city
-                    ? `<span class="storelocator-detailStoreCity">${store.city}</span>`
-                : ``}
-                ${store.phone
-                    ? `<span class="storelocator-detailStorePhone"><a href="tel:${store.phone}" title="Call">${store.phone}</a></span>`
-                : ``}
-            </div>
-        </li>`
+		<li class="storelocator-sidebarResultsListItem" data-category="${store.category}">
+			<div class="storelocator-detailStore">
+				${store.title
+					? `<span class="storelocator-detailStoreTitle"><a href="" title="See on the map" class="store-center-marker-js" data-marker-index="${store.index}">${store.index + 1}. <span>${store.title}</span></a></span>`
+				: ``}
+				<a href="http://www.google.fr/maps/dir/${origin}/${store.lat},${store.lng}" title="See the itinerary on Google Maps" target="_blank" class="storelocator-detailStoreDistance">
+					<span>${store.distance.toFixed(2)}km</span>
+					${svgRoute}
+				</a>
+				${store.address
+					? `<span class="storelocator-detailStoreAddress">${store.address}</span>`
+				: ``}
+				${store.zipcode
+					? `<span class="storelocator-detailStoreZipcode">${store.zipcode}</span>`
+				: ``}
+				${store.city
+					? `<span class="storelocator-detailStoreCity">${store.city}</span>`
+				: ``}
+				${store.phone
+					? `<span class="storelocator-detailStorePhone"><a href="tel:${store.phone}" title="Call">${store.phone}</a></span>`
+				: ``}
+			</div>
+		</li>`
 }
